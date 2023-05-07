3fbc101918a450c35984735d159d296211e751b4
Pooja Verma <pooja32verma@gmail.com>
2023-05-07 17:43:37 +0530

    Modified Digital Clock

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function current_time(){
     }
     
     hours=(hours<10) ? `0${hours}` : hours;
-    minutes=(minutes<10)? `0:${minutes}` : minutes;
-    seconds=(seconds<10) ? `0:${seconds}` : seconds;
+    minutes=(minutes<10)? `0${minutes}` : minutes;
+    seconds=(seconds<10) ? `0${seconds}` : seconds;
 
     let time = `
         <div>
